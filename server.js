ff18309fbf6c6d7783b94bb8cea654d84f5a4535
Sumit Joshi <sumitjoshi479@gmail.com>
2017-07-15 12:47:28 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({
+    port: 8080
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, 'public/index.html');
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data)
+    });
+}
+
+wss.on('connection', (ws) => {
+    ws.on('message', (msg) => {
+        data = JSON.parse(msg);
+        if(data.message)
+            wss.broadcast('<strong>'+data.name + '</strong>' + data.message);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('3000 running');
+})
