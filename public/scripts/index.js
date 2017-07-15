a8b3d5ca97b3068a648e0781343c054a6af2181c
Sumit Joshi <sumitjoshi479@gmail.com>
2017-07-15 15:16:32 +0530

    added css and completed client side connecion

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,6 @@
 const ws = new WebSocket('ws://localhost:8080');
 
-$('form').submit(function() {
+$('form').submit(function () {
     name = $('#name').val() ? $('#name').val() : 'Guest';
     $('#name-div').hide();
     $('#welcome').text(name);
@@ -8,5 +8,21 @@ $('form').submit(function() {
     ws.send(JSON.stringify({
         name: name,
         message: $('#message').val()
-    })); 
-})
+    }));
+    $('#message').focus();
+    $('#message').val('');
+    return false;
+});
+
+ws.onmessage = function (e) {
+    $('#messages').append(
+        $('<li>').html(e.data)
+    );
+};
+
+ws.onerror = function (e) {
+    $('#messages').append(
+        $('<li>').text('<span style="color: red;">Error</span>' + e.data)
+    );
+};
+
